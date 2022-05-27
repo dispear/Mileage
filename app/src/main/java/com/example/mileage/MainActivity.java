a767f8be0a9ed53fa8f102b8df33738b0e45bb57
dispear <87793524+dispear@users.noreply.github.com>
2022-05-28 00:27:09 +0900

    second day

--- app/src/main/java/com/example/mileage/MainActivity.java
+++ app/src/main/java/com/example/mileage/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.event_challenge);
+        setContentView(R.layout.check_attendance);
 
     }
 }
