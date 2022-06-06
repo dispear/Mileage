81c901621ce1a0f8b2cb52db5b0d4735afa8d99d
dispear <87793524+dispear@users.noreply.github.com>
2022-06-06 16:00:42 +0900

    notice

--- app/src/main/java/com/example/mileage/notice.java
+++ app/src/main/java/com/example/mileage/notice.java
@@ -1,17 +1,9 @@
 package com.example.mileage;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class notice extends Activity {
 
