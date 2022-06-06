d8de5d2fb23d01548377c2c357716bb4071c0274
xlloew <chan1757@naver.com>
2022-06-06 16:37:29 +0900

    gallery

--- app/src/main/java/com/example/mileage/writing.java
+++ app/src/main/java/com/example/mileage/writing.java
@@ -1,15 +1,19 @@
 package com.example.mileage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 public class writing extends Activity {
 
     ImageButton btnback;
+    ImageView ivpicture;
+    private static final int REQUEST_CODE=0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,10 +21,23 @@ public class writing extends Activity {
         setContentView(R.layout.writing);
 
         btnback = (ImageButton) findViewById(R.id.btn_back);
+        ivpicture = (ImageView) findViewById(R.id.iv_picture);
+
         btnback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        ivpicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+
             }
         });
     }
