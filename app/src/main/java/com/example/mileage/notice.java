49441d0d53172094e2e9b7517d66d3fe8223e1be
dispear <87793524+dispear@users.noreply.github.com>
2022-06-06 16:24:06 +0900

    notice

--- app/src/main/java/com/example/mileage/notice.java
+++ app/src/main/java/com/example/mileage/notice.java
@@ -1,13 +1,15 @@
 package com.example.mileage;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class notice extends Activity {
 
-    ImageButton btn_back;
+    ImageButton btn_back, ibLink1, ibLink2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,29 @@ public class notice extends Activity {
         setContentView(R.layout.notice);
 
         btn_back = (ImageButton) findViewById(R.id.btn_back);
+        ibLink1 = (ImageButton) findViewById(R.id.ib_link1);
+        ibLink2 = (ImageButton) findViewById(R.id.ib_link2);
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        ibLink1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://blog.naver.com/seoulfamilyc/222756296821"));
+                startActivity(intent);
+            }
+        });
+
+        ibLink2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://blog.naver.com/seoulkfem/222756495474"));
+                startActivity(intent);
             }
         });
     }
