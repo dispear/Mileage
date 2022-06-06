f1bf2fb372494473fa3b231e2b1aa1939097dec1
dispear <87793524+dispear@users.noreply.github.com>
2022-06-06 15:59:38 +0900

    notice

--- app/src/main/java/com/example/mileage/notice.java
+++ app/src/main/java/com/example/mileage/notice.java
@@ -2,20 +2,14 @@ package com.example.mileage;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class notice extends Activity {
 
-    ImageButton btn_back;
+    ImageButton btn_back, ibLink1, ibLink2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +17,29 @@ public class notice extends Activity {
         setContentView(R.layout.notice);
 
         btn_back = (ImageButton) findViewById(R.id.btn_back);
+        ibLink1 = (ImageButton) findViewById(R.id.ib_link1);
+        ibLink2 = (ImageButton) findViewById(R.id.ib_link2);
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        ibLink1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://blog.naver.com/nne78735/222718565046"));
+                startActivity(intent);
+            }
+        });
+
+        ibLink2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://blog.naver.com/seoulfamilyc/222756296821"));
+                startActivity(intent);
             }
         });
     }
