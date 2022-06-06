921542efbddd7a3bbd67ea56a72fcd0d9efb4d8a
xlloew <chan1757@naver.com>
2022-06-06 15:56:01 +0900

    underbardeletefix

--- app/src/main/java/com/example/mileage/notice.java
+++ app/src/main/java/com/example/mileage/notice.java
@@ -15,15 +15,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class notice extends Activity {
 
-    ImageButton btn_back;
+    ImageButton btnback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notice);
 
-        btn_back = (ImageButton) findViewById(R.id.btn_back);
-        btn_back.setOnClickListener(new View.OnClickListener() {
+        btnback = (ImageButton) findViewById(R.id.btn_back);
+        btnback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
