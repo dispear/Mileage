7935679008325b1bc2ba21c569e9fcb7c34bb735
dispear <87793524+dispear@users.noreply.github.com>
2022-06-10 15:19:36 +0900

    Login

--- app/src/main/java/com/example/mileage/KakaoApplication.java
+++ app/src/main/java/com/example/mileage/KakaoApplication.java
@@ -8,6 +8,6 @@ public class KakaoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        KakaoSdk.init(this, "5591a45ed7c371ed386ccca9c663ec6e");
+        KakaoSdk.init(this, "49cfa7f550afa77b33051786bd934cee");
     }
 }
