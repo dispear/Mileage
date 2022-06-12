6ef62fb44dcbbb2d3afe30fe2866e4774b0e9518
dispear <87793524+dispear@users.noreply.github.com>
2022-06-12 16:02:46 +0900

    challenge

--- app/src/main/java/com/example/mileage/Challenge.java
+++ app/src/main/java/com/example/mileage/Challenge.java
@@ -1,23 +1,57 @@
 package com.example.mileage;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 public class Challenge extends Activity {
-    ImageButton btnback;
+    ImageButton btnBack;
+    Button btnDaily1, btnDaily2, btnDaily3, btnWeekly1, btnWeekly2, btnWeekly3;
+    ProgressBar pb1, pb2, pb3, pb4, pb5, pb6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.challenge);
 
-        btnback = (ImageButton) findViewById(R.id.btn_back);
-        btnback.setOnClickListener(new View.OnClickListener() {
+        btnDaily1 = (Button) findViewById(R.id.btn_daily1);
+        btnDaily2 = (Button) findViewById(R.id.btn_daily2);
+        btnDaily3 = (Button) findViewById(R.id.btn_daily3);
+        btnWeekly1 = (Button) findViewById(R.id.btn_weakly1);
+        btnWeekly2 = (Button) findViewById(R.id.btn_weakly2);
+        btnWeekly3 = (Button) findViewById(R.id.btn_weakly3);
+        pb1 = (ProgressBar) findViewById(R.id.progressBar1);
+        pb2 = (ProgressBar) findViewById(R.id.progressBar2);
+        pb3 = (ProgressBar) findViewById(R.id.progressBar3);
+        pb4 = (ProgressBar) findViewById(R.id.progressBar4);
+        pb5 = (ProgressBar) findViewById(R.id.progressBar5);
+        pb6 = (ProgressBar) findViewById(R.id.progressBar6);
+
+        btnBack = (ImageButton) findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        if (pb1.getProgress() == 100) {
+            btnDaily1.setEnabled(true);
+            btnDaily1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(Challenge.this);
+                    dlg.setMessage("챌린지 완료, 100 마일리지 획득");
+                    dlg.setPositiveButton("확인",null);
+                    dlg.show();
+                }
+            });
+            btnDaily1.setEnabled(false);
+            btnDaily1.setText("이미 완료한 챌린지");
+        }
     }
 }
