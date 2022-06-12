4c732d2e255471f99dda0b5ab187a71bee4c8d10
dispear <87793524+dispear@users.noreply.github.com>
2022-06-12 16:18:11 +0900

    challenge

--- app/src/main/java/com/example/mileage/Challenge.java
+++ app/src/main/java/com/example/mileage/Challenge.java
@@ -2,6 +2,7 @@ package com.example.mileage;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,12 +47,16 @@ public class Challenge extends Activity {
                 public void onClick(View view) {
                     AlertDialog.Builder dlg = new AlertDialog.Builder(Challenge.this);
                     dlg.setMessage("챌린지 완료, 100 마일리지 획득");
-                    dlg.setPositiveButton("확인",null);
+                    dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            btnDaily1.setEnabled(false);
+                            btnDaily1.setText("이미 완료한 챌린지");
+                        }
+                    });
                     dlg.show();
                 }
             });
-            btnDaily1.setEnabled(false);
-            btnDaily1.setText("이미 완료한 챌린지");
         }
     }
 }
