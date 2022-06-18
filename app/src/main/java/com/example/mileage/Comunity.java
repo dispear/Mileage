40bcf24be95f858805b24772b69d8039ad0f3e7b
xlloew <chan1757@naver.com>
2022-06-18 23:35:17 +0900

    comnj

--- app/src/main/java/com/example/mileage/Comunity.java
+++ app/src/main/java/com/example/mileage/Comunity.java
@@ -5,31 +5,110 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ImageButton;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Comunity extends Fragment {
 
     View view;
-    ImageButton ibWrite;
-    TextView post1;
     SearchView searchView;
+    FloatingActionButton Fabwrite;
 
+    private RecyclerView comunityview;
+    private MainAdapter adapter;
+    private List<Board> boardList;
+
+    private class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
+
+        private List<Board> boardList;
+
+        public MainAdapter(List<Board> boardList){
+            this.boardList = boardList;
+        }
+
+        @NonNull
+        @Override
+        public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+            Board data = boardList.get(position);
+            holder.TitleTextView.setText(data.getTitle());
+            holder.NameTextView.setText(data.getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return boardList.size();
+        }
+
+        class MainViewHolder extends RecyclerView.ViewHolder {
+            private TextView TitleTextView;
+            private TextView NameTextView;
+
+            public MainViewHolder(View itemView) {
+                super(itemView);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getActivity(), Checklist.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intent);
+                    }
+                });
+                TitleTextView = (TextView) itemView.findViewById(R.id.itemTitle);
+                NameTextView = (TextView) itemView.findViewById(R.id.itemName);
+
+            }
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // 프레그먼트 화면 뷰를 객체화
         view = inflater.inflate(R.layout.fragment_comunity, container, false);
 
-        ibWrite = (ImageButton) view.findViewById(R.id.ib_write);
-        post1 = (TextView) view.findViewById(R.id.tv_post1);
+        Fabwrite = (FloatingActionButton) view.findViewById(R.id.Floating1);
+        comunityview = (RecyclerView) view.findViewById(R.id.C_list);
+        comunityview.setHasFixedSize(true);
+        comunityview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        comunityview.setAdapter(adapter);
+        boardList = new ArrayList<>();
+        boardList.add(new Board(null, "반갑습니다.",null,"android"));
+        boardList.add(new Board(null, "Hello",null,"server"));
+        boardList.add(new Board(null, "OK",null,"java"));
+        boardList.add(new Board(null, "안녕하세요",null,"php"));
+        boardList.add(new Board(null, "zz",null,"c"));
+        boardList.add(new Board(null, "ㅎㅎ",null,"ㅠㅠ"));
+        boardList.add(new Board(null, "질문",null,"사용자"));
+        boardList.add(new Board(null, "ㅎㅇ",null,"ㄷㄷ"));
+        boardList.add(new Board(null, "분리수거",null,"ppp"));
+        boardList.add(new Board(null, "배고파요",null,"hu"));
+        boardList.add(new Board(null, "보고싶읍니다",null,"부자"));
+
+
+        adapter = new MainAdapter(boardList);
+        comunityview.setAdapter(adapter);
 
         // 글 작성 버튼
-        ibWrite.setOnClickListener(new View.OnClickListener() {
+        Fabwrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), Writing.class);
@@ -38,12 +117,13 @@ public class Comunity extends Fragment {
             }
         });
 
-        // searchview 검색 키워드 지정
+
+                // searchview 검색 키워드 지정
         searchView = view.findViewById(R.id.csearch);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                if (s.equals("질문")) {
+                if (s.equals("OK")) {
                     Intent intent = new Intent(getActivity(), Checklist.class);
                     startActivity(intent);
                 }
@@ -57,7 +137,7 @@ public class Comunity extends Fragment {
         });
 
         // 게시글 클릭시 화면 전환
-        post1.setOnClickListener(new View.OnClickListener() {
+        comunityview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), Checklist.class);
@@ -66,5 +146,8 @@ public class Comunity extends Fragment {
             }
         });
         return view;
+
+
+        }
     }
-}
+
--- app/src/main/java/com/example/mileage/Writing.java
+++ app/src/main/java/com/example/mileage/Writing.java
@@ -4,15 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class Writing extends Activity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Writing extends Activity implements View.OnClickListener {
+    FirebaseFirestore Dbase = FirebaseFirestore.getInstance();
+    EditText writeTitle, writeContent;
     ImageButton btnBack;
     ImageView ivPicture;
+    String id;
     private static final int REQUEST_CODE = 0;
 
     @Override
@@ -20,8 +33,13 @@ public class Writing extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.writing);
 
+        writeContent = (EditText) findViewById(R.id.edt_content);
+        writeTitle = (EditText) findViewById(R.id.edt_title);
         btnBack = (ImageButton) findViewById(R.id.btn_back);
         ivPicture = (ImageView) findViewById(R.id.iv_picture);
+
+        findViewById(R.id.btn_register).setOnClickListener(this);
+
 
         // 뒤로가기 버튼
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -41,5 +59,38 @@ public class Writing extends Activity {
                 startActivityForResult(intent, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Map<String, Object> post = new HashMap<>();
+        post.put("id",id);
+        post.put("title",writeTitle.getText().toString());
+        post.put("content",writeContent.getText().toString());
+
+        Dbase.collection("board").add(post).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(Writing.this,"업로드 성공",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Writing.this,"업로드 실패",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Dbase.collection("board").document(id).set(post)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Writing.this,"업로드 성공",Toast.LENGTH_SHORT).show();
+                    }
+            }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Writing.this,"업로드 실패",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
